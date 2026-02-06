3413bbf557c0b508ac228b7d93cc7d4ae8da0238
Vinicius Ribeiro <vinir14.p5@gmail.com>
2026-02-05 21:22:59 -0300

    branch da feature publicada e AuthController concluída

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,16 +1,64 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const jwtConfig = require('../config/jwt');
+const User = require('../models/User');
 
-const login = (req, res) => {
-  const usuario = {}; // TODO: validar no banco (email/senha) e obter usuário
+class AuthController{
+async login(req, res){
+ const { email, senha } = req.body;
 
-  const token = jwt.sign(
-    { id: usuario.id, role: usuario.role },
-    jwtConfig.secret,
-    { expiresIn: jwtConfig.expiresIn }
-  );
+ if(!email || !senha){
+  return res.status(400).json({ erro: 'Email e senha são obrigatórios'});
+ }
 
-  return res.json({ token });
-};
+ const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ if(!emailValido.test(email)){
+  return res.status(400).json({ erro: 'Email inválido'});
+ }
 
-module.exports = { login };
+ const usuario = await User.findOne({
+  where: { 
+    email,
+    deleted_at: null
+  }
+
+ });
+
+ if(!usuario){
+  return res.status(404).json({ erro : 'Usuário não existe.'});
+ }
+
+ const senhaValida = await bcrypt.compare(senha, usuario.senha);
+ if (!senhaValida){
+  return res.status(401).json({ erro: 'Senha inválida'});
+ }
+
+ const token = jwt.sign(
+  { id: usuario.id, email: usuario.email },
+  jwtConfig.secret,
+  { expiresIn: jwtConfig.expiresIn }
+ );
+
+ return res.json({token});
+
+}
+
+async me(req, res){
+  const usuario = await User.findByPk(req.userId, {
+    attributes: ['id', 'nome', 'email', 'created_at']
+  });
+
+  if (!usuario){
+    return res.status(404).json({ erro: 'Usuario não existe'})
+  }
+
+  return res.json(usuario);
+}
+
+logout(req, res){
+  return res.json({ message: 'Logout realizado com sucesso'});
+}
+
+}
+
+module.exports = new AuthController();
